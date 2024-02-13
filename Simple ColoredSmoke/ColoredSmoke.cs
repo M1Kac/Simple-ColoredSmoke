6cf920bfb396da6f1dffe20991460db86d539866
M1K@c <110233336+M1Kac@users.noreply.github.com>
2024-02-13 16:30:45 +0100

    Add files via upload

--- Simple ColoredSmoke/ColoredSmoke.cs
+++ Simple ColoredSmoke/ColoredSmoke.cs
@@ -1,16 +1,28 @@
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Entities;
+using CounterStrikeSharp.API.Modules.Admin;
+using System.Text.Json.Serialization;
 
 
-namespace ColoreddSmoke;
+namespace ColoredSmoke;
 
-public partial class ColoreddSmoke : BasePlugin
+public class ConfigGen : BasePluginConfig
+{
+    [JsonPropertyName("Enabled")] public bool Enabled { get; set; } = true;
+    [JsonPropertyName("Flag")] public string Flag { get; set; } = ""; 
+    [JsonPropertyName("Color")] public string Color { get; set; } = ""; 
+}
+
+public partial class ColoreddSmoke : BasePlugin, IPluginConfig<ConfigGen>
 {
     public override string ModuleName => "ColoredSmoke";
     public override string ModuleAuthor => "M1k@c";
     public override string ModuleDescription => "ColoredSmoke";
-    public override string ModuleVersion => "V. 1.0.0";
+    public override string ModuleVersion => "V. 1.0.1";
+
+    public ConfigGen Config { get; set; } = null!;
+    public void OnConfigParsed(ConfigGen config) { Config = config; }
 
     public int Round;
     public int ConnectedPlayers;
@@ -19,16 +31,16 @@ public partial class ColoreddSmoke : BasePlugin
     public override void Load(bool hotReload)
     {
         RegisterListener<Listeners.OnEntitySpawned>(OnEntitySpawned);
-
         RegisterListener<Listeners.OnTick>(() =>
         {
             for (int i = 1; i < Server.MaxPlayers; i++)
-            {
+            {            
                 var ent = NativeAPI.GetEntityFromIndex(i);
                 if (ent == 0)
                     continue;
 
                 var client = new CCSPlayerController(ent);
+                if (Config.Flag != "" && !AdminManager.PlayerHasPermissions(client, Config.Flag)) return;
                 if (client == null || !client.IsValid)
                     continue;
             }
@@ -45,6 +57,7 @@ public partial class ColoreddSmoke : BasePlugin
 
         }
     }
+    
 
     private void OnEntitySpawned(CEntityInstance entity)
     {
@@ -53,15 +66,38 @@ public partial class ColoreddSmoke : BasePlugin
         var smokeGrenadeEntity = new CSmokeGrenadeProjectile(entity.Handle);
         if (smokeGrenadeEntity.Handle == IntPtr.Zero) return;
 
-        Server.NextFrame(() =>
+        if (Config.Enabled)
         {
+          Server.NextFrame(() =>
+          {
             var entityIndex = smokeGrenadeEntity.Thrower.Value.Controller.Value.Index;
 
             if (entityIndex == null) return;
-            if (entityIndex == 0) return;
-            smokeGrenadeEntity.SmokeColor.X = Random.Shared.NextSingle() * 255.0f;
-            smokeGrenadeEntity.SmokeColor.Y = Random.Shared.NextSingle() * 255.0f;
-            smokeGrenadeEntity.SmokeColor.Z = Random.Shared.NextSingle() * 255.0f;
-        });
+            if (Config.Color == "red")
+            {
+              smokeGrenadeEntity.SmokeColor.X = 255.0f;
+              smokeGrenadeEntity.SmokeColor.Y = 0.0f;
+              smokeGrenadeEntity.SmokeColor.Z = 0.0f;
+            }
+            if (Config.Color == "blue")
+            {
+              smokeGrenadeEntity.SmokeColor.X = 0.0f;
+              smokeGrenadeEntity.SmokeColor.Y = 0.0f;
+              smokeGrenadeEntity.SmokeColor.Z = 255.0f;
+            }
+            if (Config.Color == "green")
+            {
+              smokeGrenadeEntity.SmokeColor.X = 0.0f;
+              smokeGrenadeEntity.SmokeColor.Y = 255.0f;
+              smokeGrenadeEntity.SmokeColor.Z = 255.0f;
+            }
+            if (Config.Color == "random")
+            {
+              smokeGrenadeEntity.SmokeColor.X = Random.Shared.NextSingle() * 255.0f;
+              smokeGrenadeEntity.SmokeColor.Y = Random.Shared.NextSingle() * 255.0f;
+              smokeGrenadeEntity.SmokeColor.Z = Random.Shared.NextSingle() * 255.0f;
+            }
+          });
+        }
     }
 }
