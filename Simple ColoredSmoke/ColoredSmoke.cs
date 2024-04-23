14e1abe18adec5e51c7f62e475a8a8e185a024d3
schwarper <75811921+schwarper@users.noreply.github.com>
2024-04-23 19:05:32 +0300

    Update ColoredSmoke.cs

--- Simple ColoredSmoke/ColoredSmoke.cs
+++ Simple ColoredSmoke/ColoredSmoke.cs
@@ -1,128 +1,121 @@
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
-using CounterStrikeSharp.API.Modules.Entities;
 using CounterStrikeSharp.API.Modules.Admin;
+using System.Globalization;
 using System.Text.Json.Serialization;
-
+using static CounterStrikeSharp.API.Core.Listeners;
 
 namespace ColoredSmoke;
 
 public class ConfigGen : BasePluginConfig
 {
     [JsonPropertyName("Enabled")] public bool Enabled { get; set; } = true;
-    [JsonPropertyName("Flag")] public string Flag { get; set; } = ""; 
-    [JsonPropertyName("Color")] public string Color { get; set; } = "random"; 
+    [JsonPropertyName("Flag")] public string Flag { get; set; } = string.Empty;
+    [JsonPropertyName("Color")] public string Color { get; set; } = "random";
 }
 
-public partial class ColoreddSmoke : BasePlugin, IPluginConfig<ConfigGen>
+public partial class ColoredSmoke : BasePlugin, IPluginConfig<ConfigGen>
 {
     public override string ModuleName => "ColoredSmoke";
     public override string ModuleAuthor => "M1k@c";
     public override string ModuleDescription => "ColoredSmoke";
     public override string ModuleVersion => "V. 1.0.2";
 
-    public ConfigGen Config { get; set; } = null!;
-    public void OnConfigParsed(ConfigGen config) { Config = config; }
+    public ConfigGen Config { get; set; } = new ConfigGen();
 
-    public int Round;
-    public int ConnectedPlayers;
-
-
-    public override void Load(bool hotReload)
+    public override void Unload(bool hotReload)
     {
-        RegisterListener<Listeners.OnEntitySpawned>(OnEntitySpawned);
-        RegisterListener<Listeners.OnTick>(() =>
+        RemoveListener<OnEntitySpawned>(OnEntitySpawned);
+    }
+
+    public void OnConfigParsed(ConfigGen config)
+    {
+        if (config.Enabled)
         {
-            for (int i = 1; i < Server.MaxPlayers; i++)
-            {            
-                var ent = NativeAPI.GetEntityFromIndex(i);
-                if (ent == 0)
-                    continue;
-
-                var client = new CCSPlayerController(ent);
-                if (client == null || !client.IsValid);
-                    continue;
-            }
-        });
-
-
-        if (hotReload)
-        {
-            RegisterListener<Listeners.OnMapStart>(name =>
-            {
-                ConnectedPlayers = 0;
-                Round = 0;
-            });
+            RegisterListener<OnEntitySpawned>(OnEntitySpawned);
         }
+
+        Config = config;
     }
 
     private void OnEntitySpawned(CEntityInstance entity)
     {
-        if (entity.DesignerName != "smokegrenade_projectile") return;
-
-        var smokeGrenadeEntity = new CSmokeGrenadeProjectile(entity.Handle);
-        if (smokeGrenadeEntity.Handle == IntPtr.Zero) return;
-
-        if (Config.Enabled)
+        if (entity.DesignerName != "smokegrenade_projectile")
         {
-          Server.NextFrame(() =>
-          {
-            var entityIndex = smokeGrenadeEntity.Thrower.Value.Controller.Value.Index;
-
-            var throwerValue = smokeGrenadeEntity.Thrower.Value;
-            if (throwerValue == null) return;
-
-            var throwerValueController = throwerValue.Controller.Value;
-            if (throwerValueController == null) return;
-
-            var controller = new CCSPlayerController(throwerValueController.Handle);
-
-            if (Config.Flag != "" && !AdminManager.PlayerHasPermissions(controller, Config.Flag)) return;
-
-            if (entityIndex == null) return;
-            if (Config.Color == "red")
-            {
-              smokeGrenadeEntity.SmokeColor.X = 255.0f;
-              smokeGrenadeEntity.SmokeColor.Y = 0.0f;
-              smokeGrenadeEntity.SmokeColor.Z = 0.0f;
-            }
-            if (Config.Color == "orange")
-            {
-              smokeGrenadeEntity.SmokeColor.X = 255.0f;
-              smokeGrenadeEntity.SmokeColor.Y = 100.0f;
-              smokeGrenadeEntity.SmokeColor.Z = 0.0f;
-            }
-            if (Config.Color == "purple")
-            {
-              smokeGrenadeEntity.SmokeColor.X = 255.0f;
-              smokeGrenadeEntity.SmokeColor.Y = 0.0f;
-              smokeGrenadeEntity.SmokeColor.Z = 100.0f;
-            }
-            if (Config.Color == "blue")
-            {
-              smokeGrenadeEntity.SmokeColor.X = 0.0f;
-              smokeGrenadeEntity.SmokeColor.Y = 0.0f;
-              smokeGrenadeEntity.SmokeColor.Z = 255.0f;
-            }
-            if (Config.Color == "green")
-            {
-              smokeGrenadeEntity.SmokeColor.X = 0.0f;
-              smokeGrenadeEntity.SmokeColor.Y = 255.0f;
-              smokeGrenadeEntity.SmokeColor.Z = 50.0f;
-            }
-            if (Config.Color == "cijan")
-            {
-              smokeGrenadeEntity.SmokeColor.X = 0.0f;
-              smokeGrenadeEntity.SmokeColor.Y = 255.0f;
-              smokeGrenadeEntity.SmokeColor.Z = 200.0f;
-            }
-            if (Config.Color == "random")
-            {
-              smokeGrenadeEntity.SmokeColor.X = Random.Shared.NextSingle() * 255.0f;
-              smokeGrenadeEntity.SmokeColor.Y = Random.Shared.NextSingle() * 255.0f;
-              smokeGrenadeEntity.SmokeColor.Z = Random.Shared.NextSingle() * 255.0f;
-            }
-          });
+            return;
         }
+
+        CSmokeGrenadeProjectile grenade = new(entity.Handle);
+
+        if (grenade.Handle == IntPtr.Zero)
+        {
+            return;
+        }
+
+        Server.NextFrame(() =>
+        {
+            CBasePlayerController? player = grenade.Thrower.Value?.Controller.Value;
+
+            if (player == null)
+            {
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(Config.Flag))
+            {
+                CCSPlayerController controller = new CCSPlayerController(player.Handle);
+
+                if (!AdminManager.PlayerHasPermissions(controller, Config.Flag))
+                {
+                    return;
+                }
+            }
+
+            string color = Config.Color;
+
+            if (color == "random")
+            {
+                grenade.SmokeColor.X = Random.Shared.NextSingle() * 255.0f;
+                grenade.SmokeColor.Y = Random.Shared.NextSingle() * 255.0f;
+                grenade.SmokeColor.Z = Random.Shared.NextSingle() * 255.0f;
+            }
+            else if (color == "teamcolor")
+            {
+                byte team = player.TeamNum;
+
+                switch (team)
+                {
+                    case 1:
+                        {
+                            grenade.SmokeColor.X = 255.0f;
+                            grenade.SmokeColor.Y = 0.0f;
+                            grenade.SmokeColor.Z = 0.0f;
+                            break;
+                        }
+                    case 2:
+                        {
+                            grenade.SmokeColor.X = 0.0f;
+                            grenade.SmokeColor.Y = 0.0f;
+                            grenade.SmokeColor.Z = 255.0f;
+                            break;
+                        }
+                    default:
+                        {
+                            grenade.SmokeColor.X = 255.0f;
+                            grenade.SmokeColor.Y = 255.0f;
+                            grenade.SmokeColor.Z = 255.0f;
+                            break;
+                        }
+                }
+            }
+            else
+            {
+                string[] colors = Config.Color.Split(' ');
+
+                grenade.SmokeColor.X = float.Parse(colors[0], CultureInfo.InvariantCulture);
+                grenade.SmokeColor.Y = float.Parse(colors[1], CultureInfo.InvariantCulture);
+                grenade.SmokeColor.Z = float.Parse(colors[2], CultureInfo.InvariantCulture);
+            }
+        });
     }
 }
